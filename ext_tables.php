7b70d0cec346302758c00b892c01b4d191446dac
Eike Starkmann <eike.starkmann@undkonsorten.com>
2019-05-21 11:27:45 +0200

    [TASK] use better templates and make flexform work

--- ext_tables.php
+++ ext_tables.php
@@ -9,7 +9,7 @@ if (!defined('TYPO3_MODE')) {
 	'Ride'
 );
 
-$pluginSignature = str_replace('_','',$_EXTKEY);
+$pluginSignature = str_replace('_','',$_EXTKEY) . '_list';
 $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature] = 'pi_flexform';
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue($pluginSignature, 'FILE:EXT:' . $_EXTKEY . '/Configuration/FlexForms/flexform_list.xml');
 
